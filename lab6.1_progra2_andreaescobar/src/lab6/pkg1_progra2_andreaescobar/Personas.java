af86e2e010c9cc3b3f24af56c7e0ff61539276b6
Andrea <andreajaninaescobar@gmail.com>
2017-09-01 14:08:17 -0600

    GUI

---
+++ lab6.1_progra2_andreaescobar/src/lab6/pkg1_progra2_andreaescobar/Personas.java
@@ -0,0 +1,88 @@
+package lab6.pkg1_progra2_andreaescobar;
+
+import java.util.Date;
+
+public class Personas {
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private Date fechanacimiento;
+    private String pais;
+    private int tel;
+    private String pass;
+
+    public Personas() {
+    }
+
+    public Personas(String nombre, String apellido, String correo, Date fechanacimiento, String pais, int tel, String pass) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.fechanacimiento = fechanacimiento;
+        this.pais = pais;
+        this.tel = tel;
+        this.pass = pass;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Date getFechanacimiento() {
+        return fechanacimiento;
+    }
+
+    public void setFechanacimiento(Date fechanacimiento) {
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "Personas{" + "nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+    
+}
